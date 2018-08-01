1e392bd9d08e52cb27e06f9213b18384d6ae5259
Neet1996 <34510064+Neet1996@users.noreply.github.com>
2018-08-02 01:06:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('4350db3555e5530136cd07b53fa4091a', None)
-channel_access_token = os.getenv('0MZ23nJcb0Rtcn4jkjdm/RjNS07Dx7zj34q2SE84mlbZbrtoGunYlxb6jDIvcYisd+gyBuzGROVx0JGTPoi3DWCQHbm8YJ5aycbWf4gAL7RGx+/b/J2Kkb75Vh7Qo2NmGwi3MDQzUYPAFmbocQypWAdB04t89/1O/w1cDnyilFU=', None)
+channel_secret = os.getenv('c761cba1a02904225aa65fbebecc0252', None)
+channel_access_token = os.getenv('VCMCvgOR7CFJzNCadRFESLUWwTvcv/aVaJbtJh4WZPzjtQ/JUTqhHx5XYjO1+wr5DYb48bq8tHiooCn66lEF6rQQKhcqwYYjl0qhOW/9JSv787k4vOXWmaszcBGjM7/CYpQs17JOBw0PvmmAgo4E0wdB04t89/1O/w1cDnyilFU=', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
